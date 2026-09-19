a867805c75c6b2cbe015d637dbf2bb0ca9e5d6c7
Omar-Hafez <o.adel@voktech.com>
2026-09-19 13:13:34 +0300

    Zoom out video on mobile

--- src/components/ScrollVideo.tsx
+++ src/components/ScrollVideo.tsx
@@ -38,7 +38,7 @@ export default function ScrollVideo() {
       <video
         ref={videoRef}
         src="/omar-scroll-video.mp4"
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full w-auto h-auto object-cover scale-[1.15]"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full w-auto h-auto object-cover scale-[1.01] md:scale-[1.15]"
         muted
         playsInline
         autoPlay
